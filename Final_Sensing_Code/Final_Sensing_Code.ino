756e9c457353e08cf25387b9a3b462d2af545492
nickShermeister <ncsherman159@gmail.com>
2017-12-13 09:41:50 -0500

    So the code is limiting so we only can play 1 note at once... We need to polish up everything mechanically so it's clean.

--- Final_Sensing_Code/Final_Sensing_Code.ino
+++ Final_Sensing_Code/Final_Sensing_Code.ino
@@ -6,32 +6,35 @@ const int zInput = A0;
 
 //Other pin constants
 int sensorPins[] = {A1, A2, A3, A4};
-int sensorBounds[] = {350, 350, 350, 475, 340, 350, 475, 600, 350, 350, 350, 350};
+int sensorBounds[] = {350, 350, 350, 400, 300, 350, 475, 500, 350, 350, 350, 350};
 int solenoidPins[] =  {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, A5};
-int sensorReads[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-int sum = 0;
-int highVal = 0;
-int highLoc = 0;
 //w b w b w w b w b w b w (key orders)
-const int arrayLength = 12;     //Number of notes in an octave
 
-//Normal variables
+
+//Misc. Variables
+int highVal = -1;   //Highest value in this check of hte IR sensors
+int highLoc = 0;    //Location in the array of sensor reads of the high val
+int sensorReads[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};   //All values of this read
+const int arrayLength = 12;     //Number of notes in an octave
 int sensorValues[12];  //Multiplexed: 0-7; Normal read-ins: 8-11
 
 
 //Setup Function
 void setup() {  
-  //mount Servos and set to 0 degrees
+  //Set up solenoid pins as outputs
   for(int i = 0; i < arrayLength; i++){
     pinMode(solenoidPins[i], OUTPUT);
     digitalWrite(solenoidPins[i], LOW);
   }
+
+  //Set up multiplexing pins as outputs
   for (int i=0; i<3; i++)
   {
     pinMode(selectPins[i], OUTPUT);
     digitalWrite(selectPins[i], LOW);
   }
- pinMode(zInput, INPUT);
+ pinMode(zInput, INPUT);    //Set up multiplexing input as an input
+ //Set up remainder of sensor pins as inputs
  for(int i = 0; i < 4; i ++)
  {
   pinMode(sensorPins[i], INPUT);
@@ -39,10 +42,10 @@ void setup() {
 }
 
 
-
+//Main loop
 void loop() {
   player();
-  delay(25);  //delay so as not to have the arduino run at its full speed (there is no point)
+  delay(50);  //delay so as not to have the arduino run at its full speed (there is no point)
 }
 
 
@@ -50,26 +53,27 @@ void loop() {
 //Main playing function
 void player() {
   
-  //First deal with multiplexer Serial.println(analogRead(zInput));
+  //Get values for multiplexed sensors and compare to the high val
   for(int i = 0; i < 8; i++){
     selectMultiplexerPin(i);
     sensorValues[i] = analogRead(zInput);
-    if (sensorValues[i] > highVal){
+    if (sensorValues[i] > highVal && sensorValues[i]>sensorBounds[i]){
       highVal = sensorValues[i];
       highLoc = i;
     }
   }
   
 
-  //Then get values for the other 4
+  //Then get values for the other 4 and compare to high val
   for (int i = 0; i < 4; i++){
     sensorValues[i+8]=analogRead(sensorPins[i]);
-    if (sensorValues[i+8] > highVal){
+    if (sensorValues[i+8] > highVal && sensorValues[i+8]>sensorBounds[i]){
       highVal = sensorValues[i+8];
       highLoc = i;
     }
   }
 
+  //Change the solenoids' states, with only the high val being HIGH and the rest LOW
   for(int i = 0; i < 12; i++){
     if(i == highLoc){
       digitalWrite(solenoidPins[i], HIGH);
@@ -78,15 +82,18 @@ void player() {
       digitalWrite(solenoidPins[i], LOW);
     }
   }
-  highLoc = 0;
-  highVal = 0;
+  //Reset the high location and value
+  highLoc = -1;
+  highVal = -1;
 }
 
+//Select the right pin for multiplexing
 void selectMultiplexerPin(int pin) {
   if (pin > 7) return; // Exit if pin is out of scope
-  for (int i=0; i<3; i++)
+  
+  for (int i=0; i<3; i++) //Loop through all 3 pins
   {
-    if (pin & (1<<i))
+    if (pin & (1<<i))     //Determine if the pin should output HIGH or LOW
       digitalWrite(selectPins[i], HIGH);
     else
       digitalWrite(selectPins[i], LOW);
